d4b1d8042d88e889e22cd2f7c008c07045c6733a
igormtx igormtx <igor.a-kuznetsov@rambler.ru>
2026-04-16 14:15:37 +0300

    sound design 1.2

--- Assets/Scripts/Click/CMenuClick.cs
+++ Assets/Scripts/Click/CMenuClick.cs
@@ -12,27 +12,49 @@ public class CMenuClick : CSoundManager, IPointerClickHandler
     {
         //Debug.Log(gameObject.name);
         CS.MainAudio = Resources.LoadAll<AudioClip>("Sounds/" + gameObject.name);
-
+        PlaySound(CS.MainAudio[(int)CS.M.a00startgame]);
         // foreach (AudioClip audio in CS.MainAudio) { Debug.Log(audio.name); }
 
         //Debug.Log(CS.MainAudio);
         //throw new NotImplementedException();
 
-        Material mat1 = new Material(Shader.Find("Specular"));
-        mat1.color = Color.yellow;
-        gameObject.GetComponent<MeshRenderer>().material = mat1;
+        if (clicked == false)
+        {
+            //Debug.Log("click");
+            clicked = true;
+            timer = timer_max;
+            //Material mat1 = new Material(Shader.Find("Specular"));
+            mat1.color = Color.yellow;
+            gameObject.GetComponent<MeshRenderer>().material = mat1;
+        }
     }
 
+    private Material mat1 = null;
+    private bool clicked = false;
+    private float timer = 0;
+    private float timer_max = 2;
+
     // Start is called before the first frame update
-    //void Start()
-    //{
-        
-    //}
+    void Start()
+    {
+        mat1 = new Material(Shader.Find("Specular"));
+    }
 
     // Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    void Update()
+    {
+        if (clicked == true)
+        {
+            timer -= Time.deltaTime;
+            //Debug.Log(timer);
+            if (timer <= 0)
+            {
+                clicked = false;
+                mat1.color = Color.gray;
+                gameObject.GetComponent<MeshRenderer>().material = mat1;
+            }
+
+        }    
+    }
 } //public class CMenuClick : CSoundManager, IPointerClickHandler
 
